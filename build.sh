4e2db2358610423395ae49fbddc2e1348bd03804
Janmm14 <computerjanimaus@yahoo.de>
2016-01-17 20:51:37 +0100

    Reword a patch; Fix some commit msg typos; Update origin-git-hash patch: Use patcher-git-hash instead of patched-git-hash.

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+patcher_commit=`git rev-parse --short HEAD`
+
 cd BungeeCord
 
 origin_commit=`git rev-parse --short origin/master`
 
 echo "============ Build BungeeCord from commit $origin_commit"
 
-mvn clean install -Dorigin_commit=$origin_commit -Dbuild.number=$BUILD_NUMBER
+mvn clean install -Dorigin_commit=$origin_commit -Dpatcher_commit=$patcher_commit -Dbuild.number=$BUILD_NUMBER
 
 cd ../
 
