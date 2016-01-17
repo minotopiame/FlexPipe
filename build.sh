ca22d79de0091efe6cb6518f0a597f4b1245414c
Janmm14 <computerjanimaus@yahoo.de>
2016-01-17 18:26:49 +0100

    Show short git hash when executing build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-echo "============ Build BungeeCord"
 
 cd BungeeCord
 
 origin_commit=`git rev-parse --short origin/master`
+
+echo "============ Build BungeeCord from commit $origin_commit"
 
 mvn clean install -Dorigin_commit=$origin_commit -Dbuild.number=$BUILD_NUMBER
 
