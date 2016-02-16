b5b49ffbee573a752d0e025a114919b1eede49e5
Janmm14 <computerjanimaus@yahoo.de>
2016-02-16 14:32:22 +0100

    Add default git user.name and user.email config for travis-ci.org

--- apply.sh
+++ apply.sh
@@ -5,6 +5,11 @@ echo ""
 
 cd BungeeCord
 
+if [[ -z $(git config --local user.name) ]]; then
+	git config --local user.name "flexpipe build script"
+	git config --local user.email "flexpipe_build_script@null.flexpipe.minotopia.me"
+fi
+
 git am -3 ../patches/*.patch
 
 cd ../
