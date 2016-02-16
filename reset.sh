941411b706099b259152c3139311ccd4d8373b34
Janmm14 <computerjanimaus@yahoo.de>
2016-02-16 20:15:59 +0100

    Do not try removing BungeeCord directory if its not there

--- reset.sh
+++ reset.sh
@@ -9,7 +9,9 @@ if [ -d "BungeeCord" ] && [[ $(ls BungeeCord) ]]; then
   cd ../
   echo "======= BungeeCord updated to latest version"
 else
-  rm -rf BungeeCord
+  if [ -d "BungeeCord" ]; then
+    rm -rfd BungeeCord
+  fi
   git clone https://github.com/SpigotMC/BungeeCord.git BungeeCord
   echo "======= BungeeCord newly cloned"
 fi
