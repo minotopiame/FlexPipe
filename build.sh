a7f5585fa692a2dbf6e8402cf1775da0757bbcf8
Janmm14 <computerjanimaus@yahoo.de>
2016-02-16 19:38:56 +0100

    Fail travis-ci.org build if one bash command fails; check if BungeeCord directory is not empty before trying to update bungee with git there.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 patcher_commit=`git rev-parse --short HEAD`
 
--- reset.sh
+++ reset.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
+#!/bin/bash -e
 echo ""
 echo "======= Setting up base BungeeCord"
 echo ""
-if [ -d "BungeeCord" ]; then
+if [ -d "BungeeCord" ] && [[ $(ls BungeeCord) ]]; then
   cd BungeeCord
   git fetch origin
   git reset --hard origin/master
   cd ../
 else
+  rm -rf BungeeCord
   git clone https://github.com/SpigotMC/BungeeCord.git BungeeCord
 fi
