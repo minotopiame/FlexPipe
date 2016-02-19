57c9aa4d414beed72d6653058bd9752d9385bf57
Janmm14 <computerjanimaus@yahoo.de>
2016-02-19 20:45:49 +0100

    reset.sh: try rm without -d if it fails with -d option

--- reset.sh
+++ reset.sh
@@ -10,7 +10,7 @@ if [ -d "BungeeCord" ] && [[ $(ls BungeeCord) ]]; then
   echo "======= BungeeCord updated to latest version"
 else
   if [ -d "BungeeCord" ]; then
-    rm -rfd BungeeCord
+    rm -rfd BungeeCord || rm -rf BungeeCord
   fi
   git clone https://github.com/SpigotMC/BungeeCord.git BungeeCord
   echo "======= BungeeCord newly cloned"
