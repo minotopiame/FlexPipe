57c2601ca5e7fe5bb27fd514b241ffe87b2bad60
Janmm14 <janmarianmeyer@gmx.de>
2016-03-09 23:47:13 +0100

    Print final version string in one line\n\nThis allowes the Jenkins description setter plugin to read the full version as it can't match multi-line.

--- build.sh
+++ build.sh
@@ -4,16 +4,19 @@ patcher_commit=`git rev-parse --short HEAD`
 
 cd BungeeCord
 
+echo ""
+echo "======= Recieving maven version..."
+
+maven_version=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -e '^[[:digit:]]')
+
 origin_commit=`git rev-parse --short origin/master`
 
-echo ""
 if [ -z "$BUILD_NUMBER" ]; then
     BUILD_NUMBER="0"
     echo "======= Set BUILD_NUMBER to 0, as it was not set";
 fi
 
-echo "======= The build number is $BUILD_NUMBER"
-echo "======= Building FlexPipe from origin commit $origin_commit with patcher commit $patcher_commit"
+echo "======= Building FlexPipe version git:$maven_version:origin-$origin_commit:patcher-$patcher_commit:$BUILD_NUMBER"
 echo ""
 
 mvn clean install -Dorigin_commit=$origin_commit -Dpatcher_commit=$patcher_commit -Dbuild.number=$BUILD_NUMBER
