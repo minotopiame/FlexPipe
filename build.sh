e1464c59c4afbd81ba3706030b76b6db4409a02e
Janmm14 <computerjanimaus@yahoo.de>
2016-04-24 14:20:12 +0200

    Uniform bash file output

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ patcher_commit=`git rev-parse --short HEAD`
 cd BungeeCord
 
 echo ""
-echo "======= Recieving maven version..."
+echo "============ Recieving maven version..."
 
 maven_version=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version -B -N | grep '^[[:digit:]]' | tail -1)
 
@@ -13,10 +13,10 @@ origin_commit=`git rev-parse --short origin/master`
 
 if [ -z "$BUILD_NUMBER" ]; then
     BUILD_NUMBER="0"
-    echo "======= Set BUILD_NUMBER to 0, as it was not set";
+    echo "============ Set BUILD_NUMBER to 0, as it was not set";
 fi
 
-echo "======= Building FlexPipe version git:$maven_version:origin-$origin_commit:patcher-$patcher_commit:$BUILD_NUMBER"
+echo "============ Building FlexPipe version git:$maven_version:origin-$origin_commit:patcher-$patcher_commit:$BUILD_NUMBER"
 echo ""
 
 mvn clean install -Dorigin_commit=$origin_commit -Dpatcher_commit=$patcher_commit -Dbuild.number=$BUILD_NUMBER -B
@@ -24,8 +24,8 @@ mvn clean install -Dorigin_commit=$origin_commit -Dpatcher_commit=$patcher_commi
 cd ../
 
 echo ""
-echo "======= FlexPipe build finished"
-echo "======= Copy final jars"
+echo "============ FlexPipe build finished"
+echo "============ Copy final jars"
 echo ""
 
 mkdir -p target/modules/ || true
@@ -35,4 +35,4 @@ cp BungeeCord/bootstrap/target/FlexPipe.jar ./target/
 cp BungeeCord/module/*/target/*.jar ./target/modules/
 
 echo ""
-echo "======= FlexPipe and module jars copied to ./target/"
+echo "============ FlexPipe and module jars copied to ./target/"
--- reset.sh
+++ reset.sh
@@ -1,17 +1,18 @@
 #!/bin/bash -e
 echo ""
-echo "======= Setting up base BungeeCord"
+echo "============ Setting up base BungeeCord"
 echo ""
 if [ -d "BungeeCord" ] && [[ $(ls BungeeCord) ]]; then
   cd BungeeCord
   git fetch origin
   git reset --hard origin/master
   cd ../
-  echo "======= BungeeCord updated to latest version"
+  echo ""
+  echo "============ BungeeCord updated to latest version"
 else
   if [ -d "BungeeCord" ]; then
     rm -rfd BungeeCord || rm -rf BungeeCord
   fi
   git clone https://github.com/SpigotMC/BungeeCord.git BungeeCord
-  echo "======= BungeeCord newly cloned"
+  echo "============ BungeeCord newly cloned"
 fi
